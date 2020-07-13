9873ccd572764d8ebf4b9aa91fbc84416350bf32
Tien Nguyen <tnguyen0393@gmail.com>
2020-07-13 18:45:51 -0500

    shop overview is its own component. collections have own component to change based on url params

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -9,7 +9,7 @@ import { selectCollections } from '../../redux/shop/shop.selectors';
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
-  <div classname='collections-overview'>
+  <div className='collections-overview'>
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
