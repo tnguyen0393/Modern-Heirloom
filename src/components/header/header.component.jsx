99c5639d166f080a6555973db62b4acef49cfe76
Tien Nguyen <tnguyen0393@gmail.com>
2020-07-14 11:03:46 -0500

    contact on header removed for deployment

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,37 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
-import { auth } from "../../firebase/firebase.utils";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
 
-import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from "../cart-dropdown/cart-dropdown.component";
-import { selectCartHidden } from "../../redux/cart/cart.selectors";
-import { selectCurrentUser } from "../../redux/user/user.selectors";
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import "./header.styles.scss";
+import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    <div className='options'>
+      <Link className='option' to='/shop'>
         shop
       </Link>
-      <Link className="option" to="/contact">
+      {/* <Link className="option" to="/contact">
         contact
-      </Link>
+      </Link> */}
 
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
+        <div className='option' onClick={() => auth.signOut()}>
           sign out
         </div>
       ) : (
-        <Link className="option" to="/signin">
+        <Link className='option' to='/signin'>
           sign in
         </Link>
       )}
