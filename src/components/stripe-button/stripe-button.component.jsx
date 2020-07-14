141d0377ed0a442b81ec9713913e53c8cd1f02d0
Tien Nguyen <tnguyen0393@gmail.com>
2020-07-14 02:19:10 -0500

    stripe integration, updated favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51H4dzIJWGYUIpja7ajJHXaTKtPcrpyODJkmT8jjxuoMli30UyH4zTQoPqbrnHtsfMkYYihjLEGXVwh5ytT4kQEKO003ifmxtmk';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Modern Heirloom'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
